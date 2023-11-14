ddf2cea868cba467906d7a58a7b6cc782bbd96a3
VictorIzquier2 <victor.izquierdo1985@gmail.com>
2023-11-14 17:58:53 +0100

    Comentarios en C

---
+++ 002 - Fundamentos/002 - Comentarios/001 - lineaunica.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[]){
+
+  // Esto es un comentario
+  // Esto es otro comentario
+  // Puedo añadir tantas líneas como quiera
+  return 0;
+
+}
---
+++ 002 - Fundamentos/002 - Comentarios/002 - error.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[]){
+
+  // Esto es un comentario
+  // Esto es otro comentario
+  // Puedo añadir tantas líneas como quiera
+  /* Esto es otra línea */
+  return 0;
+
+}
